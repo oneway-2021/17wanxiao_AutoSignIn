033af6762c27a0ba78810827e2adb9f07124bcc9
llkhs <llkhs@qq.com>
2021-01-22 02:29:54 +0800

    commit message

---
+++ README.md
@@ -0,0 +1,111 @@
+# 💧广东水利电力学院完美校园签到
+😀**2021.01.22 打卡信息自动填写为上一次打卡的信息，Secret部分只需要填[账号和密码]即可**
+
+😀**2021.01.07 对应学校的模板更新，添加寒假去向三项**
+
+😀**2020.12.01 需要指定监护人名字，请从Secret字段添加监护人信息，格式看下面。**
+
+😁**2020.11.29 修改了提交接口。**
+
+**💧在[原Project](https://github.com/srcrs/Perfect_Campus_AutoSignIn)的基础下添加对[广东水利电力学院](https://www.gdsdxy.edu.cn/)的可用性，对 [srcrs](https://github.com/srcrs)的贡献表示感谢。**
+
+欢迎大家 fork 测试使用，如果可用的话，可以开 [issue](https://github.com/llkhs/Perfect_Campus_AutoSignIn/issues) 让更多人知道
+
+感谢 [@zhongbr](https://github.com/zhongbr) 的完美校园逆向登录分析代码的分享：[完美校园模拟登录](https://github.com/zhongbr/wanmei_campus)
+
+
+
+# 简介
+
+广东水利电力学院完美校园每日自动签到，从此让你解放双手，支持多用户批量签到。
+基于Github Actions，无需服务器或设备。
+
+
+# 功能
+
+- 完美校园签到
+
+- 支持多用户批量签到
+
+- 支持推送运行结果至微信(使用`server`酱)
+
+- 随机温度，随机经纬度(在合适的范围内)
+
+# 使用方法
+
+## 准备
+
+- 完美校园`APP`账号(需要手机号和密码,若未使用过，需在健康打卡页面完善个人信息)
+
+- 监护人手机号
+
+- 若需推送至微信，请注册 [server酱](http://sc.ftqq.com/)，并获取其`SCKEY`
+
+## 1.Fork本项目
+
+
+
+## 2.开启Antions(默认你的Actions是处于禁用状态)
+
+
+
+
+## 3.将个人信息添加至Secrets
+
+Name | Value
+-|-
+USERS | 手机号,密码,SCKEY
+
+多用户的`Value`格式如下：
+
+```sh
+手机号1,密码1,SCKEY
+手机号2,密码2,
+手机号3,密码3
+                    .
+                    .
+                    .
+```
+
+如需使用 [server酱](http://sc.ftqq.com/) 推送至微信，按照官网教程注册，获取其`SCKEY`。只需要在第一个用户的后面添加即可。
+
+如不需请留空，注意不要将其前面的`,`删除了。
+
+
+
+## 4.第一次运行actions
+
+只需要一个`push`操作就会触发`actions`运行
+
+将项目`poinMe.txt`文件中的`flag`值由`0`改为`1`即可。
+
+```patch
+- flag: 0
++ flag: 1
+```
+
+
+
+## 大功告成
+
+以后每天会在6:00进行自动签到。(会延迟 5 到 20分钟)
+
+
+## 如果你来自其它学校，可能某些参数不一样而打卡失败，可以试着抓包看看打卡时请求的表单，再修改对应的参数。
+ ![](https://github.com/llkhs/17wanxiao_AutoSignIn/blob/main/assets/img/E7537C28-185B-4E55-887D-1B56BD78C7F0.jpeg)
+
+## About
+- 免责声明：此仓库仅用于我个人储存17wanxiaoCheckin打卡记录用，仅供个人研究/学习/欣赏，他人可参考，请勿用于商业用途，否则产生的一切后果将由您自己承担，例如他人盗用本仓库代码用于商业用途或侵犯国家、社会、企业利益和公民的合法权益等。再次我本人不承担任何责任，此仓库仅用于我个人储存代码，仅此而已）
+
+
+
+
+## 参考项目
+
+[HAUT_autoCheck](https://github.com/YooKing/HAUT_autoCheck)
+
+[wanmei_campus](https://github.com/zhongbr/wanmei_campus)
+
+[Perfect_Campus_AutoSignIn](https://github.com/srcrs/Perfect_Campus_AutoSignIn)
+
+[17wanxiaoCheckin-Actions](https://github.com/ReaJason/17wanxiaoCheckin-Actions)
---
+++ main.py
@@ -0,0 +1,189 @@
+import time
+import json
+import requests
+import random
+import datetime
+import logging
+import os
+import sys
+from campus import CampusCard
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.chrome.options import Options
+
+chrome_options = Options()
+chrome_options.add_argument('--headless')
+chrome_options.add_argument('--no-sandbox')
+chrome_options.add_argument('--disable-dev-shm-usage')
+driver = webdriver.Chrome ('/usr/bin/chromedriver', chrome_options=chrome_options)
+# driver = webdriver.Chrome('/usr/local/bin/chromedriver', options=chrome_options)
+
+
+def initLogging():
+    logging.getLogger().setLevel(logging.INFO)
+    logging.basicConfig(format="[%(levelname)s]; %(message)s")
+
+
+def main():
+    # sectets字段录入
+    sckey, success, failure, result, phone, password = [], [], [], [], [], []
+    # 多人循环录入
+    while True:
+        try:
+            users = input()
+            info = users.split(',')
+            phone.append(info[0])
+            password.append(info[1])
+            sckey.append(info[2])
+        except BaseException:
+            break
+
+    # 提交打卡
+    for index, value in enumerate(phone):
+        print("开始获取用户%s信息" % (value[-4:]))
+        count = 0
+        while count < 3:
+            try:
+                campus = CampusCard(phone[index], password[index])
+                token = campus.user_info["sessionId"]
+                driver.get('https://reportedh5.17wanxiao.com/collegeHealthPunch/index.html?token=%s#/punch?punchId=180'%token)
+                # time.sleep(10)
+                res = check_in(token)
+                strTime = GetNowTime()
+                if res['code'] == '10000':
+                    success.append(value[-4:])
+                    logging.info(value[-4:] + "打卡成功-" + strTime)
+                    result = res
+                    break
+                else:
+                    failure.append(value[-4:])
+                    logging.info(value[-4:] + "打卡异常-" + strTime)
+                    count = count + 1
+                    print('%s打卡失败，开始第%d次重试...' % (value[-6:], count))
+                    time.sleep(30)
+
+            except Exception as err:
+                print(err)
+                logging.warning('出现错误')
+                failure.append(value[-4:])
+                break
+        print("-----------------------")
+    fail = sorted(set(failure), key=failure.index)
+    strTime = GetNowTime()
+    title = "成功: %s 人,失败: %s 人" % (len(success), len(fail))
+    try:
+        if len(sckey[0]) > 2:
+            print('主用户开始微信推送...')
+            WechatPush(
+                title,
+                'https://sc.ftqq.com/' +
+                sckey[0] +
+                '.send',
+                success,
+                fail,
+                result)
+    except BaseException:
+        print("微信推送出错！%s")
+
+
+# 时间函数
+def GetNowTime():
+    cstTime = (datetime.datetime.utcnow() + datetime.timedelta(hours=8))
+    strTime = cstTime.strftime("%H:%M:%S")
+    return strTime
+
+
+# 打卡参数配置函数
+def GetUserJson(token):
+    sign_url = "https://reportedh5.17wanxiao.com/sass/api/epmpics"
+    user_json = {
+        "jsonData": {
+            "templateid": "pneumonia",
+            "token": token
+        },
+        "businessType": "epmpics",
+        "method": "userComeApp"
+    }
+    res = requests.post(sign_url, json=user_json).json()
+    #print(res)
+    data = json.loads(res['data'])
+    #print(data)
+    post_dict = {
+        "add": data['add'],
+        "areaStr": data['areaStr'],
+        "businessType": "epmpics",
+        "method": "submitUpInfo",
+        "jsonData": {
+            "deptid": data['deptStr']['deptid'],
+            "gpsType": 1,
+            "userid": data['userid'],
+            "stuNo": data['stuNo'],
+            "source": "app",
+            "templateid": data['templateid'],
+            "token": token,
+            "reportdate": round(time.time() * 1000),
+            "username": data['username'],
+            "phonenum": data['phonenum'],
+            "customerid": data['customerid'],
+            "deptStr": data['deptStr'],
+            "areaStr": data['areaStr'],
+            "updatainfo": [{"propertyname": i["propertyname"], "value": i["value"]}
+                           for i in data['cusTemplateRelations']],
+        }
+    }
+    #print(post_dict)
+    return post_dict
+
+
+# 打卡提交函数
+def check_in(token):
+    sign_url = "https://reportedh5.17wanxiao.com/sass/api/epmpics"
+    jsons = GetUserJson(token)
+    #print(jsons)
+    # 提交打卡
+    res = requests.post(sign_url, json=jsons, timeout=10).json()
+    #print(res)
+    return res
+
+
+# 微信通知
+
+def WechatPush(title, sckey, success, fail, result):
+    strTime = GetNowTime()
+    page = json.dumps(
+        result.json(),
+        sort_keys=True,
+        indent=4,
+        separators=(
+            ',',
+            ': '),
+        ensure_ascii=False)
+    content = f"""
+        `{strTime}`
+        #### 打卡成功用户：
+        `{success}`
+        #### 打卡失败用户:
+        `{fail}`
+        #### 主用户打卡信息:
+        ```
+        {page}
+        ```
+             """
+    data = {
+        "text": title,
+        "desp": content
+    }
+    try:
+        req = requests.post(sckey, data=data)
+        if req.json()["errmsg"] == 'success':
+            print("Server酱推送服务成功")
+        else:
+            print("Server酱推送服务失败")
+    except BaseException:
+        print("微信推送参数错误")
+
+
+if __name__ == '__main__':
+    main()
